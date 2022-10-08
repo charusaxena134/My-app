3a8528df6df9e4a5b17c433bc2759271e92f8ad6
charusaxena134 <charusaxena134@gmail.com>
2022-10-08 12:21:32 +0530

    creating router

--- src/components/About.js
+++ src/components/About.js
@@ -1,127 +1,106 @@
-import React, { useState } from "react";
+import React from "react";
 
-export default function About() {
-  const [myStyle, setMyStyle] = useState({
-    color: "black",
-    backgroundColor: "white",
-  });
-
-  const [btntext, setBtnText] = useState("Enable dark mode");
-
-  const toggleStyle = () => {
-    if (myStyle.color === "black") {
-      setMyStyle({
-        color: "white",
-        backgroundColor: "black",
-        border: "1px solid white",
-      });
-      setBtnText("Enable light mode");
-    } else {
-      setMyStyle({
-        color: "black",
-        backgroundColor: "white",
-      });
-      setBtnText("Enable dark mode");
-    }
+export default function About(props) {
+  // const [myStyle, setMyStyle] = useState({
+  //     color: 'black',
+  //     backgroundColor: 'white'
+  // })
+  let myStyle = {
+    color: props.mode === "dark" ? "white" : "#042743",
+    backgroundColor: props.mode === "dark" ? "rgb(36 74 104)" : "white",
   };
+
   return (
-    <div className="container my-3" style={myStyle}>
-      <h1>About Us</h1>
-      <div
-        className="accordion accordion-flush"
-        id="accordionFlushExample"
-        style={myStyle}
+    <div className="container">
+      <h1
+        className="my-3"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
       >
+        About Us
+      </h1>
+      <div className="accordion" id="accordionExample">
         <div className="accordion-item">
-          <h2 className="accordion-header" id="flush-headingOne">
+          <h2 className="accordion-header" id="headingOne">
             <button
-              className="accordion-button collapsed"
+              className="accordion-button"
               type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#flush-collapseOne"
-              aria-expanded="false"
-              aria-controls="flush-collapseOne"
               style={myStyle}
+              data-bs-toggle="collapse"
+              data-bs-target="#collapseOne"
+              aria-expanded="true"
+              aria-controls="collapseOne"
             >
-              Accordion Item #1
+              <strong>Analyze Your text </strong>
             </button>
           </h2>
           <div
-            id="flush-collapseOne"
-            className="accordion-collapse collapse"
-            aria-labelledby="flush-headingOne"
-            data-bs-parent="#accordionFlushExample"
+            id="collapseOne"
+            className="accordion-collapse collapse show"
+            aria-labelledby="headingOne"
+            data-bs-parent="#accordionExample"
           >
-            <div className="accordion-body">
-              Placeholder content for this accordion, which is intended to
-              demonstrate the <code>.accordion-flush</code> className. This is
-              the first item's accordion body.
+            <div className="accordion-body" style={myStyle}>
+              Textutils gives you a way to analyze your text quickly and
+              efficiently. Be it word count, character count or
             </div>
           </div>
         </div>
         <div className="accordion-item">
-          <h2 className="accordion-header" id="flush-headingTwo">
+          <h2 className="accordion-header" id="headingTwo">
             <button
               className="accordion-button collapsed"
+              style={myStyle}
               type="button"
               data-bs-toggle="collapse"
-              data-bs-target="#flush-collapseTwo"
+              data-bs-target="#collapseTwo"
               aria-expanded="false"
-              aria-controls="flush-collapseTwo"
-              style={myStyle}
+              aria-controls="collapseTwo"
             >
-              Accordion Item #2
+              <strong>Free to use </strong>
             </button>
           </h2>
           <div
-            id="flush-collapseTwo"
+            id="collapseTwo"
             className="accordion-collapse collapse"
-            aria-labelledby="flush-headingTwo"
-            data-bs-parent="#accordionFlushExample"
+            aria-labelledby="headingTwo"
+            data-bs-parent="#accordionExample"
           >
-            <div className="accordion-body">
-              Placeholder content for this accordion, which is intended to
-              demonstrate the <code>.accordion-flush</code> className. This is
-              the second item's accordion body. Let's imagine this being filled
-              with some actual content.
+            <div className="accordion-body" style={myStyle}>
+              TextUtils is a free character counter tool that provides instant
+              character count & word count statistics for a given text.
+              TextUtils reports the number of words and characters. Thus it is
+              suitable for writing text with word/ character limit.
             </div>
           </div>
         </div>
         <div className="accordion-item">
-          <h2 className="accordion-header" id="flush-headingThree">
+          <h2 className="accordion-header" id="headingThree">
             <button
               className="accordion-button collapsed"
+              style={myStyle}
               type="button"
               data-bs-toggle="collapse"
-              data-bs-target="#flush-collapseThree"
+              data-bs-target="#collapseThree"
               aria-expanded="false"
-              aria-controls="flush-collapseThree"
-              style={myStyle}
+              aria-controls="collapseThree"
             >
-              Accordion Item #3
+              <strong>Browser Compatible </strong>
             </button>
           </h2>
           <div
-            id="flush-collapseThree"
+            id="collapseThree"
             className="accordion-collapse collapse"
-            aria-labelledby="flush-headingThree"
-            data-bs-parent="#accordionFlushExample"
+            aria-labelledby="headingThree"
+            data-bs-parent="#accordionExample"
           >
-            <div className="accordion-body">
-              Placeholder content for this accordion, which is intended to
-              demonstrate the <code>.accordion-flush</code> className. This is
-              the third item's accordion body. Nothing more exciting happening
-              here in terms of content, but just filling up the space to make it
-              look, at least at first glance, a bit more representative of how
-              this would look in a real-world application.
+            <div className="accordion-body" style={myStyle}>
+              This word counter software works in any web browsers such as
+              Chrome, Firefox, Internet Explorer, Safari, Opera. It suits to
+              count characters in facebook, blog, books, excel document, pdf
+              document, essays, etc.
             </div>
           </div>
         </div>
-      </div>
-      <div classNameName="container my-3">
-        <button onClick={toggleStyle} className="btn btn-primary">
-          {btntext}
-        </button>
       </div>
     </div>
   );
